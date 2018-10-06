f18d9726ef2a6f805b4fd9120f66a8131e48526b
Alex Tolkachev <freekickr@ya.ru>
2018-10-07 01:43:21 +0300

    new code

--- src/DirectionReduction/DirRed.java
+++ src/DirectionReduction/DirRed.java
@@ -1,4 +1,73 @@
 package DirectionReduction;
 
+/*
+Once upon a time, on a way through the old wild west,
+a man was given directions to go from one point to another.
+The directions were "NORTH", "SOUTH", "WEST", "EAST".
+Clearly "NORTH" and "SOUTH" are opposite, "WEST" and "EAST" too.
+Going to one direction and coming back the opposite direction is a needless effort.
+Since this is the wild west, with dreadfull weather and not much water,
+it's important to save yourself some energy, otherwise you might die of thirst!
+*/
+
+/*
+for (int i = 0; i + 1 < dirs.size(); i++) {
+    if ("NORTHSOUTH,SOUTHNORTH,EASTWEST,WESTEAST".contains(dirs.get(i) + dirs.get(i + 1))) {
+        dirs.remove(i + 1);
+        dirs.remove(i);
+        i = -1;
+    }
+*/
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+
 public class DirRed {
+    public static String[] dirReduc(String[] arr) {
+        // Your code here.
+        List<String> directions = new ArrayList<>(Arrays.asList(arr));
+        System.out.println(directions);
+        int point = opposites(directions);
+        while (point != 0) {
+            for (int i = 0; i < directions.size() - 1;) {
+                if (!checkTwo(directions.get(i), directions.get(i+1))) {
+                    directions.remove(i);
+                    directions.remove(i);
+                    continue;
+                }
+                i++;
+            }
+            point = opposites(directions);
+        }
+        System.out.println(directions);
+        return directions.toArray(new String[0]);
+    }
+
+    private static boolean checkTwo(String  dirOne, String dirTwo) {
+        if ("EAST".equals(dirOne) && !"WEST".equals(dirTwo))
+            return true;
+        if ("WEST".equals(dirOne) && !"EAST".equals(dirTwo))
+            return true;
+        if ("SOUTH".equals(dirOne) && !"NORTH".equals(dirTwo))
+            return true;
+        if ("NORTH".equals(dirOne) && !"SOUTH".equals(dirTwo))
+            return true;
+        return false;
+    }
+
+    private static int opposites(List<String> list) {
+        int res = 0;
+        for (int i = 0; i < list.size() - 1; i++) {
+            if (!checkTwo(list.get(i), list.get(i+1))) {
+                res++;
+            }
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        dirReduc(new String[] { "NORTH", "SOUTH", "SOUTH", "EAST", "WEST", "NORTH", "WEST" });
+    }
 }
--- src/TransformToPrime/TransformToPrime.java
+++ src/TransformToPrime/TransformToPrime.java
@@ -1,4 +1,44 @@
 package TransformToPrime;
 
+/*
+Task :
+Given a List [] of n integers , find minimum mumber to be inserted in a list, so that sum of all elements of list should equal the closest prime number .
+
+Notes
+List size is at least 2 .
+List's numbers will only positives (n > 0) .
+Repeatition of numbers in the list could occur .
+The newer list's sum should equal the closest prime number .
+
+minimumNumber ({3,1,2}) ==> return (1)
+minimumNumber ({2,12,8,4,6}) ==> return (5)
+minimumNumber ({50,39,49,6,17,28}) ==> return (2)
+*/
+
 public class TransformToPrime {
+    public static void main(String[] args) {
+        System.out.println(minimumNumber(new int[]{50,39,49,6,17,28}));
+    }
+
+    public static int minimumNumber(int[] numbers)
+    {
+        int sum = 0;
+        for (int n : numbers)
+            sum+=n;
+        if (isPrime(sum))
+            return 0;
+        for (int i = sum;i <= Integer.MAX_VALUE;i++) {
+            if (isPrime(i)) {
+                return (i - sum);
+            }
+        }
+        return -1; // Do your magic!
+    }
+
+    private static boolean isPrime(int n) {
+        for (int i = 2; i < n; i++)
+            if (n%i == 0)
+                return false;
+        return true;
+    }
 }
